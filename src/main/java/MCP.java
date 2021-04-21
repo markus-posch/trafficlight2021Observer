2ef2f54357d90189eab493b42a9d1303ef8f07c2
markus-posch <markus.posch1@stud.fh-campuswien.ac.at>
2021-04-21 17:34:54 +0200

    Forgot Github Repo Link

--- src/main/java/MCP.java
+++ src/main/java/MCP.java
@@ -1,6 +1,8 @@
 import trafficlight.ctrl.TrafficLightCtrl;
 
-
+/*
+Github Repo Link: https://github.com/markus-posch/trafficlight2021Observer
+ */
 public class MCP {
     public static void main(String[] args) {
         TrafficLightCtrl ctrl = TrafficLightCtrl.getInstance();
